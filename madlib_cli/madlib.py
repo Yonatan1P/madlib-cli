21ce3d8ce786af71eacd49d3f7d1d36b99b0eb6a
Yonatan Palagashvili <yonimotek@gmail.com>
2020-11-11 20:53:17 -0800

    did part of the lab, but I am stuck and not sure how to do testing

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,8 +1,26 @@
-welcome = "Welcome to Mad Python, Python Madlib Game.\n
-        "Like any Madlib, you will be prompted to choose\n
-        "some fun adjectives, adverbs, nouns, and verbs\n
-        "at random. These words will be used to fill in\n
-        "blanks in a pre-existing story. If you choose\n
-        "the right words, it may just make you slither\n
-        "right out of your skin."
+welcome="Hello! Welcome to the our python madlib game!\
+        \nYou will be prompted to choose some adjectives,\
+        \nnouns, and verbs to fill in our funny story!"
 print(welcome)
+promptAdj=input(">Adjective:")
+promptFirstName=input(">A first name:")
+promptPastTenseVerb=input(">A past tense verb:")
+promptPluralVerb=input(">A plural verb:")
+promptSmallNumber=input(">A number between 1 & 50:")
+promptfirstName=input(">A first name:")
+import re
+file_path='madlib_cli/template_madlib.txt'
+def read_file(file_path):
+    if not file_path:
+        return None
+
+    template=''
+    with open(file_path) as file:
+        for line in file:
+            template +=line
+    print(template)
+
+def parse_file(template):
+    words=[]
+    words+=template.splice(' ')
+    print(words)
